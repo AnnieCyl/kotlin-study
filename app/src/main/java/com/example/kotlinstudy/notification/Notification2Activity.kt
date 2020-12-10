4b539981a03893059cece35793850eee81f9d2b7
Annie <chenyanl@landicorp.com>
2020-12-10 22:08:11 +0800

    Notification 练习

---
+++ app/src/main/java/com/example/kotlinstudy/notification/Notification2Activity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinstudy.notification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notification2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification2)
+    }
+}
